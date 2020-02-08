f581623aeb53d5d12aa33c10110cd9a240478d20
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2020-02-08 16:38:28 -0600

    Add list items to lists endpoint

--- models/list.go
+++ models/list.go
@@ -7,23 +7,34 @@ import (
 )
 
 type List struct {
-	ID      uint32 `sql:"primary_key" json:"id" alias:"lists.id"`
+	ID      uint32 `json:"id" alias:"lists.id"`
 	GroupID uint32 `json:"-" alias:"lists.group_id"`
 	Name    string `json:"name" alias:"lists.name"`
+
+	ListItems []struct {
+		ID      uint32 `json:"id"`
+		Content string `json:"content"`
+		Checked bool   `json:"checked"`
+	} `json:"items" alias:"list_items.*"`
 }
 
 func GetAllListsForUser(userId uint32) ([]List, error) {
 	var lists []List
 
-	err := Lists.SELECT(Lists.AllColumns).FROM(
+	err := Lists.SELECT(
+		Lists.ID,
+		Lists.GroupID,
+		Lists.Name,
+		ListItems.ID,
+		ListItems.Checked,
+		ListItems.Content,
+	).FROM(
 		Lists.
+			LEFT_JOIN(ListItems, ListItems.ListID.EQ(Lists.ID)).
 			INNER_JOIN(Groups, Groups.ID.EQ(Lists.GroupID)).
 			INNER_JOIN(GroupsUsers, GroupsUsers.GroupID.EQ(Groups.ID)).
 			INNER_JOIN(Users, Users.ID.EQ(GroupsUsers.UserID))).
 		WHERE(Users.ID.EQ(mysql.Int(int64(userId)))).Query(db.GetDB().DB, &lists)
 
-	if len(lists) == 0 {
-		return make([]List, 0), err
-	}
 	return lists, err
 }
